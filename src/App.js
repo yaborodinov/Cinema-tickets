c24a6dd7f5aab6160d70fd92aeea9e9fd3ad057c
Alexandr <yaborodinov@gmail.com>
2021-08-17 09:43:12 +0300

    testing styled-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
+import styled from "styled-components";
 import './App.css';
+
+
+
+
+const AppWrapper = styled.div`
+width:100 %;
+min-height:100vh;
+padding:2rem;
+background:#ccc;
+`;
+
+const Title = styled.h1 `
+  color: ${props=>(props.red?"red":"blue")}
+`;
+
 
 function App() {
   return (
-    <div>
-      <h1>Бронирование билетов</h1>
+    < AppWrapper>
+      < Title red > Booking Tickets </Title>
       < Button variant="primary"> Primary </Button>
-    </div>
+    </AppWrapper>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import  {
+  createGlobalStyle
+} from "styled-components";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+const Global = createGlobalStyle`
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+  font-family:consolas;
+}
+`
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Global/>
+      <App/>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
