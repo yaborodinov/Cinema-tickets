f8871fbc8aafbd4d92d347863708db166b989cef
Alexandr <yaborodinov@gmail.com>
2021-08-17 15:12:55 +0300

    learning styled-components. create animations and usage default props for styled comp-s

--- src/components/CreatedButton.jsx
+++ src/components/CreatedButton.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 
-const StyledButton = styled("button")`
+
+
+const rotateAnimation = keyframes`
+  0%{
+    transform:rotateZ(0deg);
+  };
+  100%{
+    transform : rotateZ(360deg)
+  };
+`;
+
+const StyledButton = styled("button").attrs(props => ({
+  outline: true,
+}))`
   border: none;
   padding: 10px 15px;
   font-size: 18px;
@@ -10,19 +23,24 @@ const StyledButton = styled("button")`
   &:focus {
     outline: none;
   }
+  &:hover {
+    animation: ${rotateAnimation} 2s infinite linear;
+  };
   align-self: ${(props) => props.align || "stretch"};
-  border-radius:5px;
+  border-radius: 5px;
 
   ${(props) =>
-    props.primary && css`
+    props.primary &&
+    css`
       color: ${(props) => props.color || "white"};
       background: ${(props) => props.background || "#494646"};
     `};
 
   ${(props) =>
-    props.outline && css`
+    props.outline &&
+    css`
       color: ${(props) => props.color || "white"};
-      border: 1px solid ${props=>props.color || "white"};
+      border: 1px solid ${(props) => props.color || "white"};
       background: transparent;
     `}
 `;
