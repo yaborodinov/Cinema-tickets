f86efb2bbcfa8b32fb9ffd3eab24a9edd4509576
Alexandr <yaborodinov@gmail.com>
2021-09-07 23:34:22 +0300

    add try catch to fetchPosts in saga

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,15 +1,21 @@
 import { takeEvery, put, call } from "redux-saga/effects";
-import { hideLoader, showLoader } from "../actions/actionCreatePost";
+import { hideLoader, showAlert, showLoader } from "../actions/actionCreatePost";
 
 export function* sagaWatcher() {
   yield takeEvery('REQUEST_POSTS', sagaWorker)
 }
 
 function* sagaWorker() {
-  yield put(showLoader())
+  try {
+    yield put(showLoader())
   const payload = yield call(fetchPosts)
   yield put({ type: 'FETCH_POSTS', payload })
   yield put(hideLoader())
+  } catch (e){
+    yield put(showAlert("чтото пошло не так"))
+    yield put(hideLoader())
+  }
+  
 }
 
 async function fetchPosts() {
