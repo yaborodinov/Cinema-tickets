e127f6da1cd3e60dd80602cd496048a1eca9ad4a
Alexandr <yaborodinov@gmail.com>
2021-09-13 20:49:05 +0300

    used useLocation()

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
-import { Route } from 'react-router-dom';
+import { Route, useLocation } from 'react-router-dom';
 
 import { Body, Popup, Header } from './components';
 import { AppWrapper } from './styled/AppWrapper';
@@ -8,16 +8,15 @@ import { AppWrapper } from './styled/AppWrapper';
 const App = () => {
   const [modalShow, setModalShow] = useState(false);
   const [currentTime, setCurrentTime] = useState([]);
-
+  const location = useLocation();
   const handlerSelectCurrentTime = (item) => {
     setCurrentTime(item)
   };
   
   useEffect(() => {
-    const url = new URL(window.location.href);
-    if (url.pathname === '/popup') {
-    setModalShow(true);
-  };
+    if (location.pathname === "/popup") {
+      setModalShow(true);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
