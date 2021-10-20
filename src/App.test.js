d6d2a3fb980cea763e1058ba83e9f00ad541f254
Alexandr <yaborodinov@gmail.com>
2021-10-20 10:52:23 +0300

    added test for App

---
+++ src/App.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import App from './App';
+import Header from './components'
+import toJson from 'enzyme-to-json';
+
+import { shallow } from 'enzyme';
+
+jest.mock('react-router-dom', () => ({
+  useLocation: jest.fn().mockReturnValue({
+    pathname: '/another-route',
+    search: '',
+    hash: '',
+    state: null,
+    key: '5nvxpbdafa',
+  }),
+}));
+
+describe('rendering components', () => {
+  it('render App component without crashing', () => {
+    shallow(<App/>)
+  })
+  
+  it('renders App component header without crashing', () => {
+    const wrapper = shallow(<App />)
+    const header = (<Header />);
+    expect(wrapper.contains(header)).toEqual(true)
+  })
+})
+
+describe('snapshots', ()=> {
+  it('Header snapshots', ()=> {
+    const tree = shallow(<Header />)
+    expect(toJson(tree)).toMatchSnapshot()
+  })
+})
