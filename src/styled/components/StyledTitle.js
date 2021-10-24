86cdf2555ffa2dc5d42954d78dea3f64a960476a
Alexandr <yaborodinov@gmail.com>
2021-10-24 09:42:12 +0300

    corrected dependencies

--- src/styled/components/StyledTitle.js
+++ src/styled/components/StyledTitle.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
+import { theme } from '../Global';
 
 export const StyledTitle = styled('h1')`
-  color: ${props => props.color || props.theme.colors.primary};
+  color: ${props => props.color || theme.colors.primary};
   text-align: center;
   font-weight: 500;
 `;
